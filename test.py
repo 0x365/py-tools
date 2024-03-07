2aa685b2767551b7e79e5950ba6d2ecc2d202225
0x365 <robert@cowlishaw.com>
2024-03-07 10:19:09 +0000

    Add dense to sparse matrix function. Works 70percent of the time

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matrix_reshape import *
+from matrix_sparsify import *
 
-grid_data = np.round(10*np.random.rand(400,6))
+grid_data = np.round(10*np.random.rand(10,6))
+print(np.array(grid_data,dtype=int))
 coord_dims = [1,2,3,4,5]
-plot_dims = [3,2]
-coords, z = matrix_reshape_from_flat(grid_data, coord_dims, comp2d=True, plot_dims=plot_dims)
+# plot_dims = [2,3]
+coords, z = sparse_to_dense(grid_data, coord_dims, comp2d=False)#, plot_dims=plot_dims)
 
-plt.contourf(coords[plot_dims[0]], coords[plot_dims[1]], z)
-plt.show()
+# plt.contourf(coords[plot_dims[0]], coords[plot_dims[1]], z)
+# plt.show()
+
+print("INBETEWEEN")
+
+# print(z)
+
+print("INBETWEEN2")
+sparse_again = dense_to_sparse(z, coords)
+
+print(np.array(sparse_again, dtype=int))
